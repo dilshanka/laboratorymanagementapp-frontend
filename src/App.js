363f59e28f986d8ed31fc7c7c0d17633012dbb74
dilshanka <ranaweera_kd_e22@engug.ruh.ac.lk>
2024-02-23 22:16:28 +0530

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Landing from "./Source/Landing/Landingpage";
+import UserLogin from "./Source/UserLogin/UserLoginpage";
+import AdminLogin from "./Source/AdminLogin/AdminLoginpage";
+import Dashboard from "./Source/Dashboard/Dashboardview";
+import DownloadReports from "./Source/DownlordReports/downlord-reports";
+import PatientRegistration from "./Source/PatientRegistration/patient-registration";
+import PatientDetails from "./Source/PatientDetails/patient-details";
+import ViewPatientDetails from "./Source/ViewPatientDetails/view-patientDetails";
+import StaffDetails from "./Source/StaffDetails/staff-details";
+import StaffRegister from "./Source/StaffRegistration/register-staff";
+import Updatepatient from "./Source/UpdatePatient/Updatepatient";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/userlogin" element={<UserLogin />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/download-reports" element={<DownloadReports />} />
+          <Route path="/register-patient" element={<PatientRegistration />} />
+          <Route path="/patient-details" element={<PatientDetails />} />
+          <Route path="/view-patient-details" element={<ViewPatientDetails />} />
+          <Route path="/view-staff-details" element={<StaffDetails />} />
+          <Route path="/register-staff" element={<StaffRegister />} />
+          <Route path="/update-patient" element={<Updatepatient />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
